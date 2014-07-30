6bdd4368a77323fe2e5e2c9cf4e45f644a5e57ff
Aaron Chambers <achambers@gmail.com>
2014-07-30 21:57:14 +0100

    Addde redis connection code but commented it out

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,17 @@
+//var redis = require('redis');
+//var url = require('url');
+//var redisUrl = url.parse('redis://localhost:6379/0');
+//var client = redis.createClient(redisUrl.port, redisUrl.hostname, { no_ready_check: true });
+//var client = redis.createClient();
+
 module.exports = function(grunt) {
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', 'Push something to redis', function() {
+    console.log("CHEESE");
+    //client.auth(redisUrl.auth.split(":")[1]);
+    //client.on('connect', function() {
+      //client.set('foo', 'bar');
+    //})
+
+    //client.end();
+  });
 };
