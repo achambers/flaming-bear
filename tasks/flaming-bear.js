3efaf4e2d8c4105df9e394932ab7b1f5cef646a3
Aaron Chambers <achambers@gmail.com>
2014-08-04 20:03:34 +0100

    Now the appName config is global to the base task

--- tasks/flaming-bear.js
+++ tasks/flaming-bear.js
@@ -2,6 +2,9 @@
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('flaming-bear', function() {
+    var name = this.name;
+    this.requiresConfig(name + '.appName');
+
     if (this.target === 'deploy') {
       var deploy = require('../lib/deploy');
       deploy.call(this, grunt);
