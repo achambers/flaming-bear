9ba92e3b569e4397151d55b204460aab78a432f7
Aaron Chambers <achambers@gmail.com>
2014-08-05 13:30:17 +0100

    Moved s3 code into release.js task

--- tasks/release.js
+++ tasks/release.js
@@ -3,10 +3,12 @@
 module.exports = function(grunt) {
   var common = require('../lib/common')(grunt);
   var files = require('../lib/files')(grunt);
+  var path = require('path');
+  var s3 = require('s3');
   var redis = require('redis');
   var RSVP = require('rsvp');
   var files;
-  var client;
+  var redisClient;
 
   grunt.registerTask('release:promote-index', function() {
     var done = this.async();
@@ -31,7 +33,7 @@ module.exports = function(grunt) {
       grunt.log.error('Error occured when promoting [' + key + ']: ' + error);
     })
     .finally(function() {
-      client.quit();
+      redisClient.quit();
       grunt.log.ok('Disconnected from redis');
       done();
     });
@@ -52,11 +54,78 @@ module.exports = function(grunt) {
       grunt.fail.fatal("Error uploading: " + error);
     })
     .finally(function() {
-      client.quit();
+      redisClient.quit();
       grunt.log.ok('Disconnected from redis');
       done();
     });
   });
+
+  grunt.registerTask('release:publish-assets', function() {
+    var done = this.async();
+
+    var distDir = grunt.config.get('distDir') || 'dist';
+    var name = this.name;
+    var options;
+    var s3Client;
+    var uploadParams;
+
+    grunt.config.requires('s3.options.accessKeyId');
+    grunt.config.requires('s3.options.accessKeySecret');
+    grunt.config.requires('s3.options.bucket');
+
+    options = grunt.config('s3.options');
+
+    s3Client = s3.createClient({
+      s3Options: {
+        accessKeyId: options.accessKeyId,
+        secretAccessKey: options.accessKeySecret
+      }
+    });
+
+    uploadParams = {
+      localDir: distDir + '/assets',
+      deleteRemoved: false,
+      s3Params: {
+        Bucket: options.bucket,
+        Prefix: 'assets',
+        ACL: 'public-read'
+      },
+      getS3Params: function(localFile, stat, callback) {
+        var filename = path.basename(localFile);
+        var s3Params = {};
+
+        if (isHiddenFile(filename)) {
+          s3Params = null;
+        }
+
+        callback(null, s3Params);
+      }
+    };
+
+    var uploader = s3Client.uploadDir(uploadParams);
+    uploader.on('error', function(error) {
+      grunt.fail.fatal('Unable to sync with s3: ' + error.stack);
+      done();
+    });
+
+    uploader.on('progress', function() {
+      grunt.log.ok('Progress: ' + uploader.progressAmount + '/' + uploader.progressTotal);
+    });
+
+    uploader.on('end', function() {
+      grunt.log.ok('Finished uploading to s3');
+
+      grunt.log.subhead('To publish the index.html, run:');
+      grunt.log.write('grunt release:publish-index');
+      console.log('');
+
+      done();
+    });
+  });
+
+  function isHiddenFile(path) {
+    return /^\./.test(path);
+  };
 
   function redisOptions(grunt) {
     var defaultRedisOptions = {
@@ -78,16 +147,16 @@ module.exports = function(grunt) {
 
   function connect(options) {
     var options = redisOptions(grunt);
-    client = redis.createClient(options.port, options.host, {
+    redisClient = redis.createClient(options.port, options.host, {
       auth_pass: options.password
     });
 
-    client.on("error", function(error) {
+    redisClient.on("error", function(error) {
       grunt.fail.fatal("Redis Error:" + error);
     });
 
     return new RSVP.Promise(function(resolve, reject) {
-      client.on("connect", function(){
+      redisClient.on("connect", function(){
         grunt.log.ok("Connected to redis");
         resolve();
       });
@@ -117,7 +186,7 @@ module.exports = function(grunt) {
 
   function get(key) {
     return new RSVP.Promise(function(resolve, reject) {
-      client.get(key, function(error, data) {
+      redisClient.get(key, function(error, data) {
         if (error) {
           reject(error);
         } else {
@@ -129,7 +198,7 @@ module.exports = function(grunt) {
 
   function set(key, data) {
     return new RSVP.Promise(function(resolve, reject) {
-      client.set(key, data, function(error, response) {
+      redisClient.set(key, data, function(error, response) {
         if (error) {
           reject(error);
         } else {
